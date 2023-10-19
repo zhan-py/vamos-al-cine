fb32271a29ad69911df89de5aeeb484edf835ad3
Pengyong Zhan <zpyseagull@163.com>
2023-10-19 16:45:11 -0400

    added 4 models

---
+++ Data/MovieCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace cine.Data
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Documentary
+    }
+}
---
+++ Models/Producer.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cine.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProfilePictureUrl { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+    }
+}
